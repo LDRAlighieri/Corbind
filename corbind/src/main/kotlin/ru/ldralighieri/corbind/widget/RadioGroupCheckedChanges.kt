929d8bc9ab3ddab9c555e46225cbc6cc0392f265
Raupov Vladimir <GLDRAlighieri@gmail.com>
2018-10-21 19:04:16 +1000

    Добавлены ивенты RadioGroupCheckedChanges

---
+++ corbind/src/main/kotlin/ru/ldralighieri/corbind/widget/RadioGroupCheckedChanges.kt
@@ -0,0 +1,77 @@
+package ru.ldralighieri.corbind.widget
+
+import android.widget.RadioGroup
+import kotlinx.coroutines.experimental.CoroutineScope
+import kotlinx.coroutines.experimental.Dispatchers
+import kotlinx.coroutines.experimental.channels.Channel
+import kotlinx.coroutines.experimental.channels.ReceiveChannel
+import kotlinx.coroutines.experimental.channels.actor
+import kotlinx.coroutines.experimental.channels.produce
+import kotlinx.coroutines.experimental.coroutineScope
+
+// -----------------------------------------------------------------------------------------------
+
+
+fun RadioGroup.checkedChanges(
+        scope: CoroutineScope,
+        action: suspend (Int) -> Unit
+) {
+    val events = scope.actor<Int>(Dispatchers.Main, Channel.CONFLATED) {
+        for (checkedId in channel) action(checkedId)
+    }
+
+    events.offer(checkedRadioButtonId)
+    setOnCheckedChangeListener(listener(events::offer))
+    events.invokeOnClose { setOnCheckedChangeListener(null) }
+}
+
+suspend fun RadioGroup.checkedChanges(
+        action: suspend (Int) -> Unit
+) = coroutineScope {
+    val events = actor<Int>(Dispatchers.Main, Channel.CONFLATED) {
+        for (checkedId in channel) action(checkedId)
+    }
+
+    events.offer(checkedRadioButtonId)
+    setOnCheckedChangeListener(listener(events::offer))
+    events.invokeOnClose { setOnCheckedChangeListener(null) }
+}
+
+
+// -----------------------------------------------------------------------------------------------
+
+
+fun RadioGroup.checkedChanges(
+        scope: CoroutineScope
+): ReceiveChannel<Int> = scope.produce(Dispatchers.Main, Channel.CONFLATED) {
+
+    offer(checkedRadioButtonId)
+    setOnCheckedChangeListener(listener(::offer))
+    invokeOnClose { setOnCheckedChangeListener(null) }
+}
+
+suspend fun RadioGroup.checkedChanges(): ReceiveChannel<Int> = coroutineScope {
+
+    produce<Int>(Dispatchers.Main, Channel.CONFLATED) {
+        offer(checkedRadioButtonId)
+        setOnCheckedChangeListener(listener(::offer))
+        invokeOnClose { setOnCheckedChangeListener(null) }
+    }
+}
+
+
+// -----------------------------------------------------------------------------------------------
+
+
+private fun listener(
+        emitter: (Int) -> Boolean
+) = object : RadioGroup.OnCheckedChangeListener {
+    private var lastChecked = -1
+
+    override fun onCheckedChanged(group: RadioGroup, checkedId: Int) {
+        if (checkedId != lastChecked) {
+            lastChecked = checkedId
+            emitter(checkedId)
+        }
+    }
+}
