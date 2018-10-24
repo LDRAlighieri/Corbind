2b8442d826695e8a3675a2051adff1929defb73f
Raupov Vladimir <GLDRAlighieri@gmail.com>
2018-10-24 17:41:33 +1000

    Добавлены ивенты ToolbarItemClicks

---
+++ corbind-appcompat/src/main/kotlin/ru/ldralighieri/corbind/appcompat/ToolbarItemClicks.kt
@@ -0,0 +1,65 @@
+package ru.ldralighieri.corbind.appcompat
+
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import kotlinx.coroutines.experimental.CoroutineScope
+import kotlinx.coroutines.experimental.Dispatchers
+import kotlinx.coroutines.experimental.channels.Channel
+import kotlinx.coroutines.experimental.channels.ReceiveChannel
+import kotlinx.coroutines.experimental.channels.actor
+import kotlinx.coroutines.experimental.channels.produce
+import kotlinx.coroutines.experimental.coroutineScope
+
+// -----------------------------------------------------------------------------------------------
+
+
+fun Toolbar.itemClicks(
+        scope: CoroutineScope,
+        action: suspend (MenuItem) -> Unit
+) {
+    val events = scope.actor<MenuItem>(Dispatchers.Main, Channel.CONFLATED) {
+        for (item in channel) action(item)
+    }
+
+    setOnMenuItemClickListener(listener(events::offer))
+    events.invokeOnClose { setOnMenuItemClickListener(null) }
+}
+
+suspend fun Toolbar.itemClicks(
+        action: suspend (MenuItem) -> Unit
+) = coroutineScope {
+    val events = actor<MenuItem>(Dispatchers.Main, Channel.CONFLATED) {
+        for (item in channel) action(item)
+    }
+
+    setOnMenuItemClickListener(listener(events::offer))
+    events.invokeOnClose { setOnMenuItemClickListener(null) }
+}
+
+
+// -----------------------------------------------------------------------------------------------
+
+
+fun Toolbar.itemClicks(
+        scope: CoroutineScope
+): ReceiveChannel<MenuItem> = scope.produce(Dispatchers.Main, Channel.CONFLATED) {
+
+    setOnMenuItemClickListener(listener(::offer))
+    invokeOnClose { setOnMenuItemClickListener(null) }
+}
+
+suspend fun Toolbar.itemClicks(): ReceiveChannel<MenuItem> = coroutineScope {
+
+    produce<MenuItem>(Dispatchers.Main, Channel.CONFLATED) {
+        setOnMenuItemClickListener(listener(::offer))
+        invokeOnClose { setOnMenuItemClickListener(null) }
+    }
+}
+
+
+// -----------------------------------------------------------------------------------------------
+
+
+private fun listener(
+        emitter: (MenuItem) -> Boolean
+) = Toolbar.OnMenuItemClickListener(emitter::invoke)
