aec335a9770f09f40e1e12d8c13961c9aa053f8c
Raupov Vladimir <GLDRAlighieri@gmail.com>
2019-01-24 11:30:47 +1000

    Добавлен ивент ChipCloseIconClicks

--- corbind-material/src/main/kotlin/ru/ldralighieri/corbind/material/ChipCloseIconClicks.kt
+++ corbind-material/src/main/kotlin/ru/ldralighieri/corbind/material/ChipCloseIconClicks.kt
@@ -1,2 +1,72 @@
+@file:Suppress("EXPERIMENTAL_API_USAGE")
+
 package ru.ldralighieri.corbind.material
 
+import android.view.View
+import androidx.annotation.CheckResult
+import com.google.android.material.chip.Chip
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.actor
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.isActive
+import ru.ldralighieri.corbind.internal.corbindReceiveChannel
+import ru.ldralighieri.corbind.internal.safeOffer
+
+// -----------------------------------------------------------------------------------------------
+
+
+fun Chip.closeIconClicks(
+        scope: CoroutineScope,
+        capacity: Int = Channel.RENDEZVOUS,
+        action: suspend () -> Unit
+) {
+
+    val events = scope.actor<Unit>(Dispatchers.Main, capacity) {
+        for (unit in channel) action()
+    }
+
+    setOnCloseIconClickListener(listener(scope, events::offer))
+    events.invokeOnClose { setOnCloseIconClickListener(null) }
+}
+
+suspend fun Chip.closeIconClicks(
+        capacity: Int = Channel.RENDEZVOUS,
+        action: suspend () -> Unit
+) = coroutineScope {
+
+    val events = actor<Unit>(Dispatchers.Main, capacity) {
+        for (unit in channel) action()
+    }
+
+    setOnCloseIconClickListener(listener(this, events::offer))
+    events.invokeOnClose { setOnCloseIconClickListener(null) }
+}
+
+// -----------------------------------------------------------------------------------------------
+
+
+@CheckResult
+fun Chip.clicks(
+        scope: CoroutineScope,
+        capacity: Int = Channel.RENDEZVOUS
+): ReceiveChannel<Unit> = corbindReceiveChannel(capacity) {
+
+    setOnCloseIconClickListener(listener(scope, ::safeOffer))
+    invokeOnClose { setOnClickListener(null) }
+}
+
+
+// -----------------------------------------------------------------------------------------------
+
+
+@CheckResult
+private fun listener(
+        scope: CoroutineScope,
+        emitter: (Unit) -> Boolean
+) = View.OnClickListener {
+
+    if (scope.isActive) { emitter(Unit) }
+}
