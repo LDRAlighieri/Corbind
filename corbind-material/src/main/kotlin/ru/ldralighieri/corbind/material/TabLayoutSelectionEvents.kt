1cca1e596d60af8fe46c2c1eca634f4958af4870
Раупов Владимир Равильевич <vrraupov@vostbank.ru>
2019-07-24 10:24:18 +1000

    Переведены material компоненты

--- corbind-material/src/main/kotlin/ru/ldralighieri/corbind/material/TabLayoutSelectionEvents.kt
+++ corbind-material/src/main/kotlin/ru/ldralighieri/corbind/material/TabLayoutSelectionEvents.kt
@@ -9,7 +9,10 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.actor
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.isActive
 import ru.ldralighieri.corbind.internal.corbindReceiveChannel
 import ru.ldralighieri.corbind.internal.safeOffer
@@ -79,11 +82,22 @@ fun TabLayout.selectionEvents(
         scope: CoroutineScope,
         capacity: Int = Channel.RENDEZVOUS
 ): ReceiveChannel<TabLayoutSelectionEvent> = corbindReceiveChannel(capacity) {
-
     setInitialValue(this@selectionEvents, ::safeOffer)
     val listener = listener(scope, this@selectionEvents, ::safeOffer)
     addOnTabSelectedListener(listener)
     invokeOnClose { removeOnTabSelectedListener(listener) }
+}
+
+
+// -----------------------------------------------------------------------------------------------
+
+
+@CheckResult
+fun TabLayout.selectionEvents(): Flow<TabLayoutSelectionEvent> = channelFlow {
+    setInitialValue(this@selectionEvents, ::offer)
+    val listener = listener(this, this@selectionEvents, ::offer)
+    addOnTabSelectedListener(listener)
+    awaitClose { removeOnTabSelectedListener(listener) }
 }
 
 
@@ -126,4 +140,5 @@ private fun listener(
     private fun onEvent(event: TabLayoutSelectionEvent) {
         if (scope.isActive) { emitter(event) }
     }
+
 }
