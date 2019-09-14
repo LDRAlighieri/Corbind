3e9929e5c25bfd0f14721b3af0b7836cf9163d8c
Raupov Vladimir <GLDRAlighieri@gmail.com>
2019-09-14 22:10:43 +1000

    change target sdk, update sample

--- sample/src/main/kotlin/ru/ldralighieri/corbind/sample/core/CorbindActivity.kt
+++ sample/src/main/kotlin/ru/ldralighieri/corbind/sample/core/CorbindActivity.kt
@@ -16,7 +16,9 @@
 
 package ru.ldralighieri.corbind.sample.core
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -33,6 +35,13 @@ abstract class CorbindActivity : AppCompatActivity(), CoroutineScope {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         job = Job()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            val view = window.decorView
+            view.systemUiVisibility = view.systemUiVisibility or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        }
     }
 
     override fun onDestroy() {
